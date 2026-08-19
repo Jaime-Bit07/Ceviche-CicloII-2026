ead5dee347789cd6ffa6928a1440a4b98a7043e3
Jaime-Bit07 <israel.chura31@upeu.edu.pe>
2026-08-19 09:06:41 -0500

    actu

---
+++ AlgoritmoGUI/src/main/java/pe/edu/upeu/algoritmogui/HelloApplication.java
@@ -0,0 +1,15 @@
+package pe.edu.upeu.algoritmogui;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class HelloApplication extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        
+    }
+}
